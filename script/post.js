f859b3ae9af22ebda3e8517a7e7e6638973bb7ec
AshmitaHazra <aashi.hazra22@gmail.com>
2020-10-05 12:19:34 +0530

    PartC Front end

--- script/post.js
+++ script/post.js
@@ -1,40 +1,38 @@
-//sign up
-var modal1 = document.getElementById("myModal1");
-// Get the button that opens the modal
-var btn1 = document.getElementById("SigninBtn");
-// Get the <span> element that closes the modal
-var span1 = document.getElementsByClassName("close")[0];
-// When the user clicks the button, open the modal 
-btn1.onclick = function() {
-  modal1.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-span1.onclick = function() {
-  modal1.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal1) {
-    modal1.style.display = "none";
+// Get the modal
+var signUpModal = document.getElementById("SignUpModel");
+var signUpBtn = document.getElementById("SignUpBtn");
+var signUpSpan = document.getElementById('CloseSignUp');
+
+var signInModal = document.getElementById("SignInModel");
+var signInBtn = document.getElementById("SignInBtn");
+var signInSpan = document.getElementById('CloseSignIn');
+
+var deleteModal = document.getElementById("myModalDel");
+var deleteBtn = document.getElementById("SignInBtn");
+
+function openModel (btn, modal) {
+  btn.onclick = function() {
+    modal.style.display = "block";
   }
 }
-//sign in
-var modal2 = document.getElementById("myModal2");
-// Get the button that opens the modal
-var btn2 = document.getElementById("SignupBtn");
-// Get the <span> element that closes the modal
-var span2 = document.getElementsByClassName("close")[1];
-// When the user clicks the button, open the modal 
-btn2.onclick = function() {
-  modal2.style.display = "block";
+
+function closeModel (span, modal) {
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
 }
-// When the user clicks on <span> (x), close the modal
-span2.onclick = function() {
-  modal2.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
+
+openModel(signInBtn, signInModal);
+openModel(signUpBtn, signUpModal);
+closeModel(signUpSpan, signUpModal);
+closeModel(signInSpan, signInModal);
+
 window.onclick = function(event) {
-  if (event.target == modal2) {
-    modal2.style.display = "none";
+  if (event.target == signUpModal) {
+    signUpModal.style.display = "none";
+  }
+
+  if (event.target == signInModal) {
+    signInModal.style.display = "none";
   }
 }
